acdcb9f5e8b4796acc853bc664da93f24a55384c
Animish Pande <63346282+animishpande@users.noreply.github.com>
2020-07-31 17:25:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fname = "Animish";
+const lname = "Pande";
+const number = 0;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello, {fname} {lname}
+    </h1>
+    <p>Your favourite number is {number}</p>
+  </div>,
+  document.getElementById("root")
+);
